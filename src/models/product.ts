3b9ce4ca85076032693cd384a4fec1eb1137917b
urmee04 <urmee04sust@gmail.com>
2025-07-17 14:29:42 -0400

    feat: add displayDetails method to format and display product info

--- src/models/product.ts
+++ src/models/product.ts
@@ -27,4 +27,15 @@ export class Product {
   getPriceWithDiscount(): number {
     return this.price * (1 - this.discountPercentage / 100);
   }
+
+  //Display product's details in formatted string
+
+  displayDetails(): string {
+    return `Id: ${this.id}
+    Title: ${this.title}
+    Price: $${this.price.toFixed(2)}
+    Discount: ${this.discountPercentage}%
+    Discounted Price: $${this.getPriceWithDiscount().toFixed(2)}
+    Category: ${this.category}`;
+  }
 }
